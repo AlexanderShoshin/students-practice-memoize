4535e494bde9099421e3d09bb294e62af486eec0
Alexander Shoshin <Alexander_Shoshin@epam.com>
2015-10-01 15:05:49 +0300

    realize memorize function

--- src/memoize.js
+++ src/memoize.js
@@ -1,5 +1,51 @@
 export { memoize };
 
 function memoize(func) {
-    return func; // TODO
+    var cache = {};
+	var targetFunc = func;
+	
+	return function () {
+		var key;
+		
+		key = calcHash(arguments);
+		if (cache[key] === undefined) {
+			cache[key] = targetFunc.apply(this, arguments);
+		}
+		
+		return cache[key];
+	};
+}
+
+function calcHash(val) {
+	if (typeof(val) === "object") {
+		return calcObjectHash(val);
+	} else {
+		return calcSimpleHash(val);
+	}
+}
+
+function calcObjectHash(objectVal) {
+	var hash, argKey;
+	
+	hash = "(";
+	for (argKey in objectVal) {
+		hash += argKey;
+		hash += ":";
+		hash += calcHash(objectVal[argKey]);
+	}
+	hash += ")";
+	
+	return hash;
+}
+
+function calcSimpleHash(simpleVal) {
+	var hash;
+	
+	hash = "(";
+	hash += simpleVal;
+	hash += ",";
+	hash += typeof(simpleVal);
+	hash += ")";
+	
+	return hash;
 }
